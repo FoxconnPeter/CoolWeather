942eb6007baaeabe622eb002412546cbed234ed1
Peter <597058061@qq.com>
2017-01-09 17:51:17 +0800

    这是我自己模范《第一行代码》写的天气APP。

--- MyApplication/app/src/main/java/com/liqipeter/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/liqipeter/myapplication/MainActivity.java
@@ -1,13 +1,33 @@
 package com.liqipeter.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getString("weather",null) != null){
+            Intent intent =new Intent(this,WeatherActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+
+
     }
 }
